b4c43a170901e7bd9d6097f4beb8334b5edf0d64
YossiBenZaken <yossibz@gmail.com>
2022-12-02 13:54:27 +0200

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Yosef Ben Zaken on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
